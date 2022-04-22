e4313c600673c2b12c3fe51e0b6f62bcc9e53031
ui-dev17 <98046751+pcuiux@users.noreply.github.com>
2022-04-22 10:51:06 +0530

    hamburger fix

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-        var sticky = $('.topNavBar'),
-            scroll = $(window).scrollTop();
-        if (scroll >= 113) {
-            sticky.addClass('sticky-header');
-        } else {
-            sticky.removeClass('sticky-header');
-        }
-    });
+    if ($(window).width() >= 1000) {
+        $(window).scroll(function () {
+            var sticky = $('.topNavBar'),
+                scroll = $(window).scrollTop();
+            if (scroll >= 113) {
+                sticky.addClass('sticky-header');
+            } else {
+                sticky.removeClass('sticky-header');
+            }
+        });
+    }
     $(".search-icon").click(function () {
         $("#headerSearch").fadeToggle();
     });
