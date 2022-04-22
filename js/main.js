663a0fc37300d1b86e35e64f8603c4bcbbfc44ea
ui-dev17 <98046751+pcuiux@users.noreply.github.com>
2022-04-22 10:28:14 +0530

    hamburger menu

--- js/main.js
+++ js/main.js
@@ -11,4 +11,21 @@ $(document).ready(function () {
     $(".search-icon").click(function () {
         $("#headerSearch").fadeToggle();
     });
+    $("#toggleNav").click(function () {
+        $("#megaMenu").slideToggle("slow", function () {
+            if ($("#megaMenu").is(":visible")) {
+                $("body").addClass("openMenu");
+            } else {
+                $("body").removeClass("openMenu");
+            }
+        });
+    });
+    $(".listItems li").click(function () {
+        if ($(this).hasClass("active")) {
+            $(".listItems li").removeClass("active")
+        } else {
+            $(".listItems li").removeClass("active");
+            $(this).addClass("active");
+        }
+    });
 });
